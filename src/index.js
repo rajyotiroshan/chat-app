b433594d03f62d5fcc0bf722c8e3c08ec99f51a5
rajan <rajangreedge@gmail.com>
2019-09-11 19:35:16 +0530

    basic setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path');
+const app = express();
+
+//PORT
+const PORT = process.env.PORT || 3000;
+
+//serve static public folder
+app.use(express.static(path.join(__dirname,'./public')));
+
+//listen for server
+app.listen(PORT,(err)=>{
+  console.log('Server started on port:: ', PORT);
+})
