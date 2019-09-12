6d0c8f9ee2b0df80a2defd99c986a51d5fcb5eaa
rajan <rajangreedge@gmail.com>
2019-09-12 20:21:18 +0530

    send location btn disables and enabled

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,21 +3,21 @@ const socket = io();
 const $messageForm = document.querySelector('#message-form');
 const $messageFormInput = document.querySelector('.msg-input');
 const $messageFormButton = document.querySelector('.msg-send');
-
+const $sendLocationButton = document.querySelector('#send-location');
 //listen for the welcome event from server.
-socket.on('welcome', (msg)=>{
+socket.on('welcome', msg => {
   console.log(msg);
-})
+});
 
 //listen for message-form submit event
-$messageForm.addEventListener("submit",(e)=>{
+$messageForm.addEventListener('submit', e => {
   e.preventDefault();
   //disable the form s.t user can not send another
   //just yet
   $messageFormButton.setAttribute('disabled', 'disabled');
   let msg = e.target.elements.message.value;
   //emits sendMessage to server
-  socket.emit('sendMessage', msg, (err)=>{
+  socket.emit('sendMessage', msg, err => {
     //enable the form again.
     $messageFormButton.removeAttribute('disabled');
     //clear form input field.
@@ -25,28 +25,40 @@ $messageForm.addEventListener("submit",(e)=>{
     //focus form input
     $messageFormInput.focus();
     //call when server listen for sendMessage.
-    if(err) {
+    if (err) {
       return console.log(err);
     }
     console.log('Message deliverede');
   });
-})
+});
 
 //listen for message event from server.
-socket.on('message', (msgStr)=>{
+socket.on('message', msgStr => {
   console.log(msgStr);
-})
+});
 
 //location
-document.querySelector('#send-location').addEventListener('click',()=>{
-  if(!navigator.geolocation) {
+document.querySelector('#send-location').addEventListener('click', () => {
+  if (!navigator.geolocation) {
     return alert('Geolocation is not supported by your browser');
   }
+  //disable send location
+  $sendLocationButton.setAttribute('disabled', 'disabled');
   //else
-  navigator.geolocation.getCurrentPosition((position)=>{
+  navigator.geolocation.getCurrentPosition(position => {
     //console.log(position);
-    socket.emit('sendLocation', {latitude: position.coords.latitude, longitude: position.coords.longitude}, (msg)=>{
-      console.log(msg);
-    });
-  })
-})
+    socket.emit(
+      'sendLocation',
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      },
+      msg => {
+        //enalbe the send location button
+        $sendLocationButton.removeAttribute('disabled');
+
+        console.log(msg);
+      }
+    );
+  });
+});
