736b7193ec8dc80f812b1ba1b6e645c4e73c2707
rajan <rajangreedge@gmail.com>
2019-09-12 17:48:09 +0530

    remove counter practice code

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,14 +1,4 @@
 
 const socket = io();
 
-//recieve countUpdated event from server
-socket.on('countUpdated', (count)=>{
-  console.log('The count is updated::--',count)
-})
 
-//click for button
-document.querySelector('#increment').addEventListener('click',()=>{
-  console.log('clicking');
-  //on click emit an event to server.
-  socket.emit('increment');
-})
--- src/index.js
+++ src/index.js
@@ -14,20 +14,11 @@ const PORT = process.env.PORT || 3000;
 //serve static public folder
 app.use(express.static(path.join(__dirname,'../public')));
 
-let count = 0;
 //listen for new client connection
 io.on('connection',(socket)=>{
   //use socket object to communicate to the connected client.
   console.log('new connection established');
-  //emit is used to send an custom/in-built event to the client
-  socket.emit('countUpdated', count);
-  //listen for client increment event
-  socket.on('increment', ()=>{
-    count++;
-    //emit the countUpdated event to the clients.
-    //socket.emit('countUpdated', count);//Note : only emitting to single connection
-    io.emit('countUpdated',count);
-  })
+
 })
 //listen for server
 server.listen(PORT,(err)=>{
