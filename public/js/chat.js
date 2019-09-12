f4fbbf06b425092abeb20d2434b1b858db752f93
rajan <rajangreedge@gmail.com>
2019-09-12 13:19:06 +0530

    emit increment event from client

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,3 +5,10 @@ const socket = io();
 socket.on('countUpdated', (count)=>{
   console.log('The count is updated::--',count)
 })
+
+//click for button
+document.querySelector('#increment').addEventListener('click',()=>{
+  console.log('clicking');
+  //on click emit an event to server.
+  socket.emit('increment');
+})
