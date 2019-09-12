8652d405805bb9546c19f9368064c149e09469fc
rajan <rajangreedge@gmail.com>
2019-09-12 12:41:36 +0530

    config to use socketio

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-
+const socketio = require('socket.io');
 const app = express();
 
 //create server
 const server = http.createServer(app);
-
+//instance on socketio to config server
+const io = socketio(server);
 //PORT
 const PORT = process.env.PORT || 3000;
 
 //serve static public folder
 app.use(express.static(path.join(__dirname,'../public')));
 
+//listen for new client connection
+io.on('connection',()=>{
+  console.log('new connection established');
+})
 //listen for server
 server.listen(PORT,(err)=>{
   console.log('Server started on port:: ', PORT);
