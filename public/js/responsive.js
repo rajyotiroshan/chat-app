1b0a542439e8d37ba2a1c49f9cea4119668af3ea
rajan <rajangreedge@gmail.com>
2019-09-16 13:01:27 +0530

    responsive fix

--- public/js/responsive.js
+++ public/js/responsive.js
@@ -1,6 +1,6 @@
 const $menuBars = document.querySelector('.fa-bars');
 const $chatSidebar = document.querySelector('.chat__sidebar');
 $menuBars.addEventListener('click', (event)=>{
-  console.log('clicked');
+  //console.log('clicked');
   $chatSidebar.classList.toggle('chat__sidebar--close');
 })
